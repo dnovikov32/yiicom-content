33f83ab29c5f5d970cc2ceda1b29b7353b857451
dn <dn@t1000.su>
2019-11-26 17:41:32 +0300

    feat(frontend): added default index page template

---
+++ src/frontend/views/page/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\web\View;
+use yiicom\content\common\models\Page;
+
+/**
+ * @var View $this
+ * @var Page $page
+ */
+
+?>
+
+<div>Default yiicom/content index page template</div>
+
+
+
+
+
+
+
+
