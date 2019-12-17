551c3d8ecec8b611d26a27c16e8b9608aa246f43
dn <dn@t1000.su>
2019-12-17 16:36:08 +0300

    feat(UrlValidator): added symbol replace _ -> -

--- src/common/validators/UrlValidator.php
+++ src/common/validators/UrlValidator.php
@@ -23,7 +23,7 @@ class UrlValidator extends Validator
             $model->$attribute = $this->filter($model->$attribute);
         }
 
-        if (!preg_match('/^[\w\-\.\/]+$/', $model->$attribute)) {
+        if (! preg_match('/^[\w\-\.\/]+$/', $model->$attribute)) {
             $model->addError($attribute, 'Неверный формат (доступны: буквы, цифры, "-", ".", "/"');
 
             return false;
@@ -40,6 +40,7 @@ class UrlValidator extends Validator
     {
         $value = mb_strtolower($value);
         $value = trim($value);
+        $value = str_replace(['_', ' '], '-', $value);
         $value = preg_replace('/(\/+)/', '/', $value);
         $value = trim($value, '/');
 
